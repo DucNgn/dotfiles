4205cf61d5364b8bf8dd28a35da908e8588b3b54
DucNgn <triduc2311m@gmail.com>
2022-11-03 16:08:47 -0400

    New Lvim config bc they decided to pull the rug with new updates

--- config.lua
+++ config.lua
@@ -1,132 +1,134 @@
--- ********
--- Settings
--- ********
-
-
-lvim.colorscheme = "arctic"
-
-vim.opt.timeoutlen = 500
-vim.opt.shortmess = vim.opt.shortmess + "I"
+-- general
+lvim.reload_config_on_save = false
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.lint_on_save = true
 vim.opt.lazyredraw = true
 vim.opt.relativenumber = true
 
-lvim.format_on_save = false
+-- theme
+vim.termguicolors = true
+lvim.colorscheme = "aquarium"
 
-lvim.builtin.alpha.active = false
-lvim.builtin.project.active = false
-lvim.builtin.telescope.defaults.prompt_prefix = " "
-lvim.builtin.telescope.defaults.path_display.shorten = nil
-lvim.builtin.telescope.pickers = {
-  find_files = { find_command = { "fd", "--type=file", "--hidden", "--exclude", ".git" } },
-}
-lvim.builtin.terminal.active = true
-lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "css",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "rust",
-  "toml",
-  "typescript",
-  "yaml",
-}
-
-
--- ***********
--- Keybindings
--- ***********
-
+-- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
-vim.cmd("map 0 ^")
-vim.cmd("nnoremap Q <nop>")
-vim.cmd("nnoremap cc ddko")
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["Q"] = "<nop>"
 
-vim.cmd("nnoremap j gj")
-vim.cmd("nnoremap k gk")
-vim.cmd("nnoremap <Leader>F <cmd>lua GrepInputString()<CR>")
-
-
-lvim.keys.normal_mode["<Leader>bn"] = ":bnext<CR>"
-lvim.keys.normal_mode["<Leader>bx"] = ":bp <BAR> bd #<CR>"
+lvim.keys.insert_mode["kj"] = "<Esc>"
+lvim.keys.insert_mode["jk"] = "<Esc>"
 
 lvim.keys.visual_block_mode.J = false
 lvim.keys.visual_block_mode.K = false
 
-function GrepInputString()
-  local default = vim.api.nvim_eval([[expand("<cword>")]])
-  local input = vim.fn.input({
-    prompt = "Search for: ",
-    default = default,
-  })
-  require("telescope.builtin").grep_string({ search = input })
-end
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
-lvim.builtin.which_key.mappings["sT"] = { "<cmd>lua GrepInputString()<CR>", "Text under cursor" }
-lvim.builtin.which_key.mappings["lo"] = { "<cmd>TSLspOrganize<CR>", "Organize imports" }
-lvim.builtin.which_key.mappings["lI"] = { "<cmd>TSLspImportAll<CR>", "Import all" }
-lvim.builtin.which_key.mappings["li"] = { "<cmd>TSLspImportCurrent<CR>", "Import under cursor" }
+-- Treesitter
+lvim.builtin.treesitter.ensure_installed = {
+  "javascript",
+  "typescript",
+  "json",
+  "lua",
+  "python",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+  "latex",
+}
+lvim.builtin.treesitter.highlight.enable = true
 
--- *******
+-- LSP
+lvim.lsp.automatic_servers_installation = false
+
+-- Formatters and Linters
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+  {
+    exe = "black",
+    filetypes = { "python" },
+    args = { "--quiet", "--fast", "-" },
+  },
+  {
+    exe = "rustfmt",
+    filetype = { "rust" },
+  },
+  { exe = "prettier" },
+  { exe = "gofmt", filetypes = { "go" } },
+  { exe = "stylua", filetypes = { "lua" } },
+})
+
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+  { exe = "flake8" },
+})
+
 -- Plugins
--- *******
-
 lvim.plugins = {
-  { "AndrewRadev/tagalong.vim" },
   {
     "folke/zen-mode.nvim",
     config = function()
-      require("zen-mode").setup { plugins = { tmux = { enabled = true } } }
+      require("zen-mode").setup {
+        window = {
+          options = {
+            number = false,
+            cursorcolumn = false,
+          }
+        },
+        plugins = { tmux = { enabled = true } }
+
+      }
     end
   },
-  { "christoomey/vim-tmux-navigator" },
-  { "editorconfig/editorconfig-vim" },
-  { "farmergreg/vim-lastplace" },
-  { "felipec/vim-sanegx", event = "BufRead" },
-  { "fenetikm/falcon" },
-  { "ggandor/lightspeed.nvim" },
-  { "jose-elias-alvarez/nvim-lsp-ts-utils" },
-  { "jxnblk/vim-mdx-js" },
-  { "tpope/vim-abolish" },
-  { "tpope/vim-repeat" },
-  { "tpope/vim-surround", keys = { "c", "d", "y" } },
   {
-    "npxbr/glow.nvim",
-    ft = { "markdown" }
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
   },
-  { "rockyzhang24/arctic.nvim", requires = { "rktjmp/lush.nvim" } }
+  {
+    "itchyny/vim-cursorword",
+    event = { "BufEnter", "BufNewFile" },
+    config = function()
+      vim.api.nvim_command("augroup user_plugin_cursorword")
+      vim.api.nvim_command("autocmd!")
+      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+      vim.api.nvim_command("augroup END")
+    end
+  },
+  { "nicknikolov/dark-matter.vim" },
+  { "FrenzyExists/aquarium-vim" },
+  { "christoomey/vim-tmux-navigator" },
+  { "p00f/nvim-ts-rainbow" },
 }
 
--- ***
--- LSP
--- ***
+-- Plugin Settings
+lvim.keys.normal_mode["<C-k>"] = "<cmd>ZenMode<cr>"
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
-
-local lspconfig = require("lspconfig")
-
-local tsserver_opts = {
-  init_options = require("nvim-lsp-ts-utils").init_options,
-  on_attach = function(client)
-    local ts_utils = require("nvim-lsp-ts-utils")
-    ts_utils.setup({})
-    ts_utils.setup_client(client)
+-- Autocmd for LaTeX
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = {
+    "*.tex"
+  },
+  callback = function()
+    vim.cmd [[
+      nnoremap <buffer> tc :!xelatex %<CR> 
+      nnoremap <buffer> tp :!open -a Skim %:r.pdf<CR><CR>
+    ]]
   end,
-}
-lspconfig["tsserver"].setup(tsserver_opts)
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { exe = "black", filetypes = { "python" } },
-  { exe = "isort", filetypes = { "python" } },
-  { exe = "prettierd" },
-}
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { exe = "flake8" },
-  { exe = "eslint_d" },
-}
+})
