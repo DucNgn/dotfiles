edad14793d76d9a047c16b748b2d93a2e915b3d9
Duc Nguyen <duc.duke.ngn@gmail.com>
2022-11-09 13:34:02 -0500

    Update script to install python with poetry

--- install_python.sh
+++ install_python.sh
@@ -19,7 +19,16 @@ export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/sqlite/lib/pkgconfig"
 echo "Done!"
 echo
-echo "Installing Python 3.8.5"
-pyenv install 3.8.5
+echo "Installing Python 3.9.15"
+pyenv install 3.9.15
+echo "Adding pyenv to path"
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 echo "Done!"
+echo
+echo "Installing and configuring Poetry"
+brew install poetry
+poetry config virtualenvs.in-project true
+echo "Done! Restart or source .zshrc for the changes to take effect"
 exit 0
